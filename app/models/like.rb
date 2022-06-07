693e64988a6c3be01d3a646179728991e35f824c
Ivan Silva <ivankuartz@hotmail.com>
2022-06-07 18:01:34 -0300

    Refactor updater for like counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,12 @@
 class Like < ApplicationRecord
-  belongs_to :user
   belongs_to :post
-  scope :update_likes_counter, ->(post, count) { post.update(likes_counter: count) }
+  belongs_to :author, class_name: 'User'
+
+  after_save :update_likes_counter
+
+  private
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
