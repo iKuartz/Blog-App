da1dea35e326053fb365885f2d50b44abb9db699
Ivan Silva <ivankuartz@hotmail.com>
2022-06-07 14:58:12 -0300

    Added method to show last 5 comments in a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+    scope :update_posts_counter, ->(user, count) { user.update(posts_counter: count) }
+  scope :last_five_comments, ->(post) { Comment.where(post:).last(5) }
 end
