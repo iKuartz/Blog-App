a634a5fee061b8b1dcc726125c2288b7d52a136a
Ivan Silva <ivankuartz@hotmail.com>
2022-06-07 18:00:43 -0300

    Refactor comments updater

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :user
   belongs_to :post
-    scope :update_comments_counter, ->(post, count) { post.update(comments_counter: count) }
+  belongs_to :author, class_name: 'User'
+
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
