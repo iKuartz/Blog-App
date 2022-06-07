3dc7690184f6193fd7e1050e81820b6c74a1a177
Ivan Silva <ivankuartz@hotmail.com>
2022-06-07 14:54:54 -0300

    Added method to return 3 posts from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :nullify
   validates :name, presence: true
+    scope :last_three_posts, ->(user) { Post.where(author: user).last(3) }
 end
