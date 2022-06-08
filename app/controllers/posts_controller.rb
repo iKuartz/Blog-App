4759f6fb256aa29a0d01428d41c6749a054b4c01
Ivan Silva <ivankuartz@hotmail.com>
2022-06-08 16:17:25 -0300

    Linter corrections

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    def index
+  def index
     @user = User.find(params[:user_id])
     @post = Post.where(author_id: @user.id).order(created_at: :desc)
   end
