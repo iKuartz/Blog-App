1d8736ea29fbae57706f7f70651310a0bb9a056b
Ivan Silva <ivankuartz@hotmail.com>
2022-06-09 20:26:59 -0300

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
-Rails.application.routes.draw do    
-  root "users#index"
-  resources :users do
-    resources :posts 
-  end
+Rails.application.routes.draw do
+  root 'users#index'
 
-  get '/users/:user_id/posts/:post_id/comment', to: 'comments#new', as: 'new_comment'
-  post '/users/:user_id/posts/:post_id/comment', to: 'comments#create' #
-  get '/users/:user_id/posts/:post_id/like', to: 'posts#show'
-  post '/users/:user_id/posts/:post_id/like', to: 'likes#create', as: 'new_like'
- 
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index new create show] do
+      resources :comments, only: %i[create]
+      resources :likes, only: %i[create]
+    end
+  end
 end
