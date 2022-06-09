6429a99ba28d8b19324a41a3ca7ae2c0f01be77a
Ivan Silva <ivankuartz@hotmail.com>
2022-06-09 20:26:41 -0300

    Database files updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-first_user = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'John Smith', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1', bio: 'Doctor from Brazil.')
-third_user = User.create(name: 'Noah', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1', bio: 'Pilot from Germany.')
-fourth_user = User.create(name: 'Ann Charlotte', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1', bio: 'Lawyer from Ghana.')
+# Create two users
+first_user = User.create(name: 'Terrosvaldo',
+                         photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+                         bio: 'Artificer from Cormyr.')
+second_user = User.create(name: 'Princess Ericha Drachensbäene',
+                          photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+                          bio: 'Princess from Damara.')
 
-# Posts
-first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-second_post = Post.create(author: first_user, title: 'Hi', text: 'This is my second post')
-third_post = Post.create(author: first_user, title: 'Hi there', text: 'This is my third post')
-fourth_post = Post.create(author: first_user, title: 'Hello!', text: 'This is my fourth post')
+# Create four posts
+first_post = Post.create(author: first_user, title: 'Hello 1 (Terrosvaldo)', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello 2', text: 'This is my second post')
+third_post = Post.create(author: first_user, title: 'Hello 3', text: 'This is my third post')
+fourth_post = Post.create(author: first_user, title: 'Hello 4', text: 'This is my fourth post')
 
-# Comments
-Comment.create(author_id: first_user, text: 'Lorem officia deserunt mollit anim id est laborum.', post_id: first_post)
-Comment.create(post_id: first_post, author_id: second_user, text: 'Hi Tom!')
-Comment.create(post_id: first_post, author_id: first_user, text: 'Great Post')
-Comment.create(post_id: second_post, author_id: fourth_user, text: 'Nice Content, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
-Comment.create(post_id: third_post, author_id: third_user, text: 'Hello there!')
-Comment.create(post_id: first_post, author_id: first_user, text: 'Hi, Lorem ipsum dolor sit amet')
-Comment.create(post_id: first_post, author_id: first_user, text: 'Hello, Lorem ipsum dolor sit amet')
-Comment.create(post_id: fourth_post, author_id: first_user, text: 'Like the post, Lorem ipsum dolor sit amet')
-Comment.create(post_id: first_post, author_id: first_user, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
-Comment.create(post_id: first_post, author_id: first_user, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+# Create comments
+Comment.create(post: first_post, author: second_user, text: 'Hi Terrosvaldo!')
+Comment.create(post: first_post, author: first_user, text: 'Hi Ericha!')
+Comment.create(post: first_post, author: second_user, text: 'How r u doing?')
+Comment.create(post: first_post, author: first_user, text: 'Great, what about u?')
+Comment.create(post: first_post, author: second_user, text: 'Pretty good')
+Comment.create(post: first_post, author: first_user, text: 'Goodbye')
+
+Comment.create(post: second_post, author: second_user, text: 'Hello!')
+Comment.create(post: second_post, author: first_user, text: 'Hello Ericha!!!')
+Comment.create(post: third_post, author: second_user, text: 'Nice post, Terrosvaldo!')
+
+# Like posts
+Like.create(post: first_post, author: first_user)
+Like.create(post: first_post, author: second_user)
+Like.create(post: second_post, author: second_user)
